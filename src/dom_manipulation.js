9d03bca53d5592b409f910398928291682c002c6
malkabaltazar <malkabaltazar@gmail.com>
2020-08-05 13:08:53 -0500

    Change variable names

--- src/dom_manipulation.js
+++ src/dom_manipulation.js
@@ -1,108 +1,108 @@
 const domManipulation = (() => {
 
-  const renderProjects = (projects) => {
-    for (var i = 0; i < projects.length; i++) {
-      addProject(projects[i]);
+  const renderlists = (lists) => {
+    for (var i = 0; i < lists.length; i++) {
+      addlist(lists[i]);
     };
   };
 
-  const addProject = (project) => {
-    let list = document.getElementById('slide-out');
+  const addlist = (list) => {
+    let ul = document.getElementById('slide-out');
     let li = document.createElement('li');
-    li.innerHTML = `<a href="#" data="${project.id}" class="sidenav-close">New Project</a>`;
-    li.addEventListener('click', function() { renderList(project); });
-    list.insertBefore(li, document.getElementById('new_project').parentNode);
+    li.innerHTML = `<a href="#" data="${list.id}" class="sidenav-close">New list</a>`;
+    li.addEventListener('click', function() { renderList(list); });
+    ul.insertBefore(li, document.getElementById('new_list').parentNode);
     return li;
   };
 
-  const renderList = (project) => {
-    renderTitle(project);
-    renderBin(project);
+  const renderList = (list) => {
+    renderTitle(list);
+    renderBin(list);
     document.getElementsByTagName('table')[0].innerHTML = ''
-    for (let i = 0; i < project.list.length; i++) {
-      renderTodo(project.list[i], project);
+    for (let i = 0; i < list.items.length; i++) {
+      renderitem(list.items[i], list);
     };
   };
 
-  const renderTitle = (project) => {
+  const renderTitle = (list) => {
     let h3 = document.getElementsByTagName('h3')[0];
-    h3.innerHTML = project.title;
+    h3.innerHTML = list.title;
     let old_edit = document.getElementById('edit');
     let edit = old_edit.cloneNode(true);
     old_edit.parentNode.replaceChild(edit, old_edit);
     edit.addEventListener('click', function() {
-      let new_title = prompt('New title:', `${project.title}`);
+      let new_title = prompt('New title:', `${list.title}`);
       if (new_title != null) {
-        project.title = new_title
-        h3.innerHTML = project.title;
-        document.querySelectorAll(`a[data='${project.id}']`)[0].innerHTML = project.title;
+        list.title = new_title
+        h3.innerHTML = list.title;
+        document.querySelectorAll(`a[data='${list.id}']`)[0].innerHTML = list.title;
       };
     });
   };
 
-  const renderBin = (project) => {
+  const renderBin = (list) => {
     let old_bin = document.getElementById('delete');
     let bin = old_bin.cloneNode(true);
     old_bin.parentNode.replaceChild(bin, old_bin);
     bin.addEventListener('click', function() {
-      if (confirm('Do you want to delete this project?') == true) {
-        project.delete();
+      if (confirm('Do you want to delete this list?') == true) {
+        list.delete();
       };
     });
   };
 
-  const renderTodo = (todo, project, i = -1) => {
+  const renderitem = (item, list, i = -1) => {
     let table = document.getElementsByTagName('table')[0];
     let row = table.insertRow(i);
-    todo.row = row;
+    item.row = row;
     let check = row.insertCell(0);
     check.innerHTML = `<input type="checkbox">`;
-    if (todo.complete == true) check.children[0].checked = true;
+    if (item.complete == true) check.children[0].checked = true;
     check.children[0].addEventListener('click', function() {
-      todo.toggle();
+      item.toggle();
     });
-    row.insertCell(1).innerHTML = `${todo.title}`;
-    row.insertCell(2).innerHTML = `${todo.description}`;
-    row.insertCell(3).innerHTML = `${todo.dueDate}`;
-    row.insertCell(4).innerHTML = `${todo.priority}`;
-    let editTodo = row.insertCell(5);
-    editTodo.innerHTML = `<a href="#modal" class="modal-trigger">edit</a>`;
-    editTodo.addEventListener('click', function() {
-      renderEditForm(todo, project);
+    row.insertCell(1).innerHTML = `${item.title}`;
+    row.insertCell(2).innerHTML = `${item.description}`;
+    row.insertCell(3).innerHTML = `${item.dueDate}`;
+    row.insertCell(4).innerHTML = `${item.priority}`;
+    let edititem = row.insertCell(5);
+    edititem.innerHTML = `<a href="#modal" class="modal-trigger">edit</a>`;
+    edititem.addEventListener('click', function() {
+      renderEditForm(item, list);
     });
-    let deleteTodo = row.insertCell(6);
-    deleteTodo.innerHTML = `<a href="#">&#x1F5D1;</a>`;
-    deleteTodo.addEventListener('click', function() {
-      if (confirm('Do you want to delete this todo item?') == true) {
-        todo.delete(project);
+    let deleteitem = row.insertCell(6);
+    deleteitem.innerHTML = `<a href="#">&#x1F5D1;</a>`;
+    deleteitem.addEventListener('click', function() {
+      if (confirm('Do you want to delete this item item?') == true) {
+        item.delete(list);
       };
     });
   };
 
-  const renderTodoForm = (project) => {
+  const renderitemForm = (list) => {
     let properties = ['title', 'description', 'dueDate', 'priority']
     for (var i = 0; i < properties.length; i++) {
       document.getElementById(properties[i]).value = '';
     }
   };
 
-  const renderEditForm = (todo, project) => {
-    document.getElementById('title').value = todo.title;
-    document.getElementById('description').value = todo.description;
-    document.getElementById('dueDate').value = todo.dueDate;
-    document.getElementById('priority').value = todo.priority;
+  const renderEditForm = (item, list) => {
+    document.getElementById('title').value = item.title;
+    document.getElementById('description').value = item.description;
+    document.getElementById('dueDate').value = item.dueDate;
+    document.getElementById('priority').value = item.priority;
     document.getElementById('submit').onclick = function() {
       M.Modal.getInstance(document.querySelectorAll(".modal")[0]).close();
-      todo.title = document.getElementById('title').value;
-      todo.description = document.getElementById('description').value;
-      todo.dueDate = document.getElementById('dueDate').value;
-      todo.priority = document.getElementById('priority').value;
-      domManipulation.renderTodo(todo, project, todo.row.rowIndex);
-      document.getElementsByTagName('table')[0].deleteRow(todo.row.rowIndex + 1);
+      item.title = document.getElementById('title').value;
+      item.description = document.getElementById('description').value;
+      item.dueDate = document.getElementById('dueDate').value;
+      item.priority = document.getElementById('priority').value;
+      domManipulation.renderitem(item, list, item.row.rowIndex);
+      document.getElementsByTagName('table')[0].deleteRow(item.row.rowIndex + 1);
     };
   };
 
-  return { renderProjects, renderList, addProject, renderTodo, renderTodoForm };
+  return { renderlists, renderList, addlist, renderitem, renderitemForm };
 })();
 
 export default domManipulation;
--- src/index.js
+++ src/index.js
@@ -7,76 +7,76 @@ document.addEventListener("DOMContentLoaded", function () {
   M.Sidenav.init(document.querySelectorAll(".sidenav"));
 });
 
-class Todo {
+class Item {
   constructor(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.complete = false;
-    // localStorage.setItem('projects', JSON.stringify(projects));
-    // this.tr = (domManipulation.renderTodo(this));
+    // localStorage.setItem('lists', JSON.stringify(lists));
+    // this.tr = (domManipulation.renderitem(this));
   };
   toggle() {
     this.complete == false ? this.complete = true : this.complete = false;
   }
-  delete(project) {
+  delete(list) {
     this.row.parentNode.removeChild(this.row);
-    project.list = project.list.filter(todo => todo !== this);
+    list.items = list.items.filter(item => item !== this);
   };
 };
 
-class Project {
-  constructor(title = 'New Project') {
+class List {
+  constructor(title = 'New list') {
     this.id = id++; // necessary?
     this.title = title;
-    this.list = [];
-    this.li = (domManipulation.addProject(this));
-    // localStorage.setItem('projects', JSON.stringify(projects));
-    // console.log(localStorage.getItem('projects'))
-    // console.log(JSON.parse(localStorage.getItem('projects')))
+    this.items = [];
+    this.li = (domManipulation.addlist(this));
+    // localStorage.setItem('lists', JSON.stringify(lists));
+    // console.log(localStorage.getItem('lists'))
+    // console.log(JSON.parse(localStorage.getItem('lists')))
   };
   static create() {
-    let newProject = new Project();
-    projects.push(newProject);
-    currentProject = newProject;
-    domManipulation.renderList(newProject);
+    let newList = new List();
+    lists.push(newList);
+    currentlist = newList;
+    domManipulation.renderList(newList);
   };
   delete() {
     this.li.parentNode.removeChild(this.li);
-    projects = projects.filter(project => project !== this);
-    projects.length > 0 ? domManipulation.renderList(projects[0]) : Project.create();
+    lists = lists.filter(list => list !== this);
+    lists.length > 0 ? domManipulation.renderList(lists[0]) : List.create();
   };
 };
 
 let id = 0;
-let currentProject;
-let projects = [];
-// let projects;
-// if (localStorage.getItem('projects')) {
-//   projects = JSON.parse(localStorage.getItem('projects'));
+let currentlist;
+let lists = [];
+// let lists;
+// if (localStorage.getItem('lists')) {
+//   lists = JSON.parse(localStorage.getItem('lists'));
 //   add reviver parameter?
 // } else {
-//   projects = [];
-//   Project.create();
+//   lists = [];
+//   List.create();
 // }
-document.getElementById('new_project').addEventListener('click', Project.create);
-document.getElementById('new_todo').addEventListener('click', function() {
-  domManipulation.renderTodoForm(currentProject);
+document.getElementById('new_list').addEventListener('click', List.create);
+document.getElementById('new_item').addEventListener('click', function() {
+  domManipulation.renderitemForm(currentlist);
   document.getElementById('submit').onclick = function() {
     M.Modal.getInstance(document.querySelectorAll(".modal")[0]).close();
     let title = document.getElementById('title').value;
     let description = document.getElementById('description').value;
     let dueDate = document.getElementById('dueDate').value;
     let priority = document.getElementById('priority').value;
-    let newTodo = new Todo(title, description, dueDate, priority);
-    currentProject.list.push(newTodo);
-    domManipulation.renderTodo(newTodo, currentProject);
+    let newItem = new Item(title, description, dueDate, priority);
+    currentlist.items.push(newItem);
+    domManipulation.renderitem(newItem, currentlist);
   };
 });
-domManipulation.renderProjects(projects);
-Project.create();
+domManipulation.renderlists(lists);
+List.create();
 
-// console.log(JSON.parse(window.localStorage.getItem('projects')))
-// console.log(JSON.parse(window.localStorage.getItem('projects')))
+// console.log(JSON.parse(window.localStorage.getItem('lists')))
+// console.log(JSON.parse(window.localStorage.getItem('lists')))
 // in terminal: npx webpack
